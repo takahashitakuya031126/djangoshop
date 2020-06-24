59e2b4a31444eb77817ba914f305e5c857add6a9
Ubuntu <ubuntu@ip-172-31-1-42.ap-northeast-1.compute.internal>
2020-06-24 05:33:05 +0000

    add_cart,delete_cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from shop.models import Product
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -20,3 +21,46 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
         pass
  
     return render(request, 'cart/cart.html', dict(cart_items = cart_items, total = total, counter = counter))
+
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+                cart_id =  _cart_id(request)
+            )
+        cart.save()
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+                product = product,
+                quantity = 1,
+                cart = cart
+            )
+        cart_item.save()
+    return redirect('cart:cart_detail')
+
+
+def cart_remove(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart:cart_detail')
+
+
+def full_remove(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart:cart_detail')
