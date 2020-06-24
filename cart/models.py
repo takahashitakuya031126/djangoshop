f672b4b4a2ef9ca5727a26aa023593abdda9bb37
Ubuntu <ubuntu@ip-172-31-1-42.ap-northeast-1.compute.internal>
2020-06-24 05:06:35 +0000

    ModelTest

--- cart/models.py
+++ cart/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from shop.models import Product
 
 class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
@@ -10,3 +11,19 @@ class Cart(models.Model):
 
     def __str__(self):
         return self.cart_id
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'CartItem'
+
+    def sub_total(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return self.product.name
--- cart/tests/test_models.py
+++ cart/tests/test_models.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 from django.utils import timezone
-from cart.models import Cart
+from shop.models import Category, Product
+from cart.models import Cart, CartItem
 
 class ModelTest(TestCase):
 
     def test_create_cart(self):
-        
         Cart.objects.create(
             cart_id='USERONLY',
             date_added=timezone.now
@@ -13,3 +13,35 @@ class ModelTest(TestCase):
 
         c = Cart.objects.all()[0]
         assert str(c) == c.cart_id
+    
+    def test_create_cartitem(self):
+        category = Category.objects.create(
+            name = 'Black Urban Cushion',
+            slug = 'black-urban-cushion',
+            description = 'This is a category for black urban cushion'
+        )
+ 
+        product = Product.objects.create(
+            name = 'pag dog',
+            slug = 'pag-dog',
+            description = 'Pag dog is as big as cats',
+            category = category,
+            price = 30.2,
+            stock = 30,
+            available = True
+        )
+ 
+        cart = Cart.objects.create(
+            cart_id='USERONLY',
+            date_added=timezone.now
+        )
+ 
+        CartItem.objects.create(
+            product = product,
+            cart = cart,
+            quantity = 3,
+            active = True
+        )
+ 
+        cart_item = CartItem.objects.all()[0]
+        assert str(cart_item) == cart_item.product.name
